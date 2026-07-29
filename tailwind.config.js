b17819959c190d9b54bde4fd8cb08dba3fe4fce5
luxmohub-art <luxmohub@gmail.com>
2026-07-30 03:12:40 +0530

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html", "./src/**/*.{js,jsx}"],
+  theme: {
+    extend: {
+      colors: {
+        base: "#0b0f19",
+      },
+    },
+  },
+  plugins: [],
+};
