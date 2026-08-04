9c0e4ff994036cd1163ac3ec6514e95ab2796b71
luxmohub-art <luxmohub@gmail.com>
2026-08-04 22:45:40 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,4 +3,20 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
+
+  server: {
+    host: true,
+    port: 5173,
+    open: true
+  },
+
+  preview: {
+    host: true,
+    port: 4173
+  },
+
+  build: {
+    outDir: "dist",
+    sourcemap: false
+  }
 });
