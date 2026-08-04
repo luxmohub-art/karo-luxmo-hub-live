c3d847e42981c5c2ad4d1f66e28f3cd1348ac464
luxmohub-art <luxmohub@gmail.com>
2026-08-04 22:53:10 +0530

    Update Firebase configuration and imports

--- src/firebase.js
+++ src/firebase.js
@@ -1,17 +1,22 @@
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
+import { getAuth } from "firebase/auth";
+import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyD366V9fIGJnHwfpgf9KYW3e...",
+  apiKey: "YOUR_CURRENT_API_KEY",
   authDomain: "luxmo-hub.firebaseapp.com",
   projectId: "luxmo-hub",
   storageBucket: "luxmo-hub.firebasestorage.app",
-  messagingSenderId: "1054383037062",
-  appId: "1:1054383037062:web:12898cc7c85be687424f07",
-  measurementId: "G-NNT3KBE925"
+  messagingSenderId: "YOUR_CURRENT_SENDER_ID",
+  appId: "YOUR_CURRENT_APP_ID",
+  measurementId: "YOUR_CURRENT_MEASUREMENT_ID"
 };
 
 const app = initializeApp(firebaseConfig);
+
 export const db = getFirestore(app);
+export const auth = getAuth(app);
+export const storage = getStorage(app);
 
-
+export default app;
