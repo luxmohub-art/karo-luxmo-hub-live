6a1ae61f3cb23543163bc5d18cd59723902d2739
luxmohub-art <luxmohub@gmail.com>
2026-08-04 22:56:03 +0530

    Update Firebase configuration with real API keys

--- src/firebase.js
+++ src/firebase.js
@@ -1,22 +1,22 @@
 import { initializeApp } from "firebase/app";
-import { getFirestore } from "firebase/firestore";
 import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
-  apiKey: "YOUR_CURRENT_API_KEY",
+  apiKey: "PASTE_YOUR_REAL_API_KEY",
   authDomain: "luxmo-hub.firebaseapp.com",
   projectId: "luxmo-hub",
   storageBucket: "luxmo-hub.firebasestorage.app",
-  messagingSenderId: "YOUR_CURRENT_SENDER_ID",
-  appId: "YOUR_CURRENT_APP_ID",
-  measurementId: "YOUR_CURRENT_MEASUREMENT_ID"
+  messagingSenderId: "PASTE_YOUR_REAL_MESSAGING_SENDER_ID",
+  appId: "PASTE_YOUR_REAL_APP_ID",
+  measurementId: "PASTE_YOUR_REAL_MEASUREMENT_ID",
 };
 
 const app = initializeApp(firebaseConfig);
 
-export const db = getFirestore(app);
 export const auth = getAuth(app);
+export const db = getFirestore(app);
 export const storage = getStorage(app);
 
 export default app;
