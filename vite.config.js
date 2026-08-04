d035610547d706f7b030cfd8adb96ece5d483c2b
luxmohub-art <luxmohub@gmail.com>
2026-08-05 00:30:57 +0530

    Add chunk size warning limit to build configuration

--- vite.config.js
+++ vite.config.js
@@ -6,8 +6,7 @@ export default defineConfig({
 
   server: {
     host: true,
-    port: 5173,
-    open: true
+    port: 5173
   },
 
   preview: {
@@ -17,6 +16,7 @@ export default defineConfig({
 
   build: {
     outDir: "dist",
-    sourcemap: false
+    sourcemap: false,
+    chunkSizeWarningLimit: 1000
   }
 });
