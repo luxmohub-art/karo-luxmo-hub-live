bdfca566fe8d59743f18a7aac4c176a075971074
luxmohub-art <luxmohub@gmail.com>
2026-08-16 20:27:21 +0530

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -1,5 +1,5 @@
 import { initializeApp } from "firebase/app";
-import { getDatabase } from "firebase/database";
+import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
   apiKey: "AIzaSyDIhrrRlRzbOm@iM1EsfMCrxglchl4IAI",
@@ -12,5 +12,5 @@ const firebaseConfig = {
 };
 
 const app = initializeApp(firebaseConfig);
-export const database = getDatabase(app);
+export const db = getFirestore(app);
 export default app;
