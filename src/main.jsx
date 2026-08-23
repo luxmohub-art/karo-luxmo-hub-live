4a6e92a9d2a7f653e40aca7e9b0d4d3a52737c40
luxmohub-art <luxmohub@gmail.com>
2026-08-23 18:38:38 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -6,10 +6,19 @@ import { Toaster } from "react-hot-toast";
 import App from "./App";
 import "./index.css";
 
-ReactDOM.createRoot(document.getElementById("root")).render(
+const rootElement = document.getElementById("root");
+
+if (!rootElement) {
+  throw new Error(
+    'Root element "#root" was not found in index.html'
+  );
+}
+
+ReactDOM.createRoot(rootElement).render(
   <React.StrictMode>
     <BrowserRouter>
       <App />
+
       <Toaster
         position="top-right"
         reverseOrder={false}
